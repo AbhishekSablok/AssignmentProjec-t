53b96065b14502fe3ea150463769c7823d2bf340
Abhishek Sablok <abhishek.sablok@techdome.net.in>
2022-01-12 10:58:38 +0530

    Assignment Project

---
+++ AssignmentProject.Model/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AssignmentProject.Model
+{
+    public class Constants
+    {
+        public static string Failure = "Failure";
+        public static string Success = "Success";
+    }
+}
